4aca0b70cdb96ac0492af532f610010f044a0cd1
SanChi117 <kriptabuchcko117@gmail.com>
2026-06-03 22:47:26 +0300

    Expose integrated pipeline modules

--- strategy_lab/__init__.py
+++ strategy_lab/__init__.py
@@ -1,8 +1,12 @@
 """Smoke Strategy Lab package."""
 
 __all__ = [
+    "config",
+    "schemas",
     "rolling_symbol_strength",
     "trade_quality_score",
     "structure_learning",
     "strategy_assembly",
+    "universe_selector",
+    "pipeline",
 ]
